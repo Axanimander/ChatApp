89c7f7ac44a9f6a068ac2fc62b7426fe27b55f58
Stephen Anderson <sanderson86@mail.csuchico.edu>
2020-12-18 04:36:23 +0000

    Fixing

--- finalproject/finalapp/views.py
+++ finalproject/finalapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.core import serializers
 import datetime
 import json
